29bbb800adc76c0d58e6ab8d56c3e56cea01966c
Jeremy Epstein <jazepstein@gmail.com>
2024-12-29 12:51:43 +1100

    update cities

--- tests/cities.test.ts
+++ tests/cities.test.ts
@@ -4,8 +4,8 @@ import { wordlist } from "../src/cities";
 describe("wordlist", () => {
   it("contains a list of 2048 words", () => {
     expect(wordlist.length).toEqual(2048);
-    expect(wordlist[0]).toEqual("aachen");
-    expect(wordlist[818]).toEqual("iquique");
-    expect(wordlist[2047]).toEqual("zwolle");
+    expect(wordlist[0]).toEqual("aalborg");
+    expect(wordlist[769]).toEqual("iquitos");
+    expect(wordlist[2047]).toEqual("zurich");
   });
 });
